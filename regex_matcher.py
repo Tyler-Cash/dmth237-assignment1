19930ffe3117af8e444e0f359cfe48397ff5419c
Tyler Cash <contact@tylercash.xyz>
2017-03-27 14:01:16 +1100

    pattern_c completed and results outputted to stdout

--- regex_matcher.py
+++ regex_matcher.py
@@ -6,6 +6,9 @@ pattern_a = r'[01]*(00000011111|11111100000)[01]*'
 #  (0 + 1)∗ 0(01)^5 1 (0 + 1)∗
 pattern_b = r'(([01]*)(0(01){5}1)([01]*))'
 
+# (0 + 1)∗ 000000(0 + 1)^14 111111 (0 + 1)∗
+pattern_c = r'(([01]*)(000000[01]{14}111111)([01]*))'
+
 
 def read_file(file_name):
     list_of_strings = []
@@ -33,6 +36,11 @@ def match_language():
     # Matches pattern for q6) b) and then prints matches
     results = [in_array + 1 for in_array, line_value in enumerate(strings) if re.match(pattern_b, line_value)]
     print('---------MATCHES FOR Q6) B)---------')
+    print_list(results)
+
+    # Matches pattern for q6) c) and then prints matches
+    results = [in_array + 1 for in_array, line_value in enumerate(strings) if re.match(pattern_c, line_value)]
+    print('---------MATCHES FOR Q6) C)---------')
     print_list(results)
 
 
